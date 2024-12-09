415e2fbf529b2a106f9276e0d5778a7c5e884718
codeysun <codey.sun@gmail.com>
2024-12-09 15:01:06 -0800

    Add undercurls to nvim and tmux

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -5,3 +5,12 @@ require("config.keymaps")
 -- Generally read .keymap files as C ones
 vim.cmd([[autocmd BufNewFile,BufRead *.keymap setfiletype c]])
 vim.cmd([[autocmd BufNewFile,BufRead *.overlay setfiletype c]])
+
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	float = { border = "rounded", source = true },
+	severity_sort = true,
+})
