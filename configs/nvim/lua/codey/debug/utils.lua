a9588100ef34aa6855d31c9624be6d9c2c1db9cd
Codey <48636667+codeysun@users.noreply.github.com>
2025-08-30 17:09:36 -0700

    wip debugger

---
+++ configs/nvim/lua/codey/debug/utils.lua
@@ -0,0 +1,62 @@
+local dap = require("dap")
+
+local dap_utils = {}
+
+---@class LaunchDebugConfig
+---@field kind string
+---@field exe string
+---@field args string[]
+
+---@type LaunchDebugConfig|nil
+dap_utils._last_exec = nil
+dap_utils._debug_kind_launchers = {}
+
+function dap_utils.debug_last()
+	if dap_utils._last_exec == nil then
+		error("last exec is nil, you must debug something once before using debug_last")
+		return
+	end
+	dap_utils.launch_debug(dap_utils._last_exec)
+end
+
+---@param conf LaunchDebugConfig
+function dap_utils.launch_debug(conf)
+	local make_config = dap_utils._debug_kind_launchers[conf.kind]
+	if make_config == nil then
+		error("no debug launcher registered for kind: " .. conf.kind)
+		return
+	end
+
+	dap_utils._last_exec = conf
+	local dap_config = make_config(conf.exe, conf.args)
+	dap.run(dap_config)
+end
+
+---@param fn fun(selected_exe:string, args:string[]|nil):dap.Configuration
+function dap_utils.register_debug_kind(name, fn)
+	dap_utils._debug_kind_launchers[name] = fn
+end
+
+-- user command: LaunchDebug <kind> <exe path> [<args to fwd to exe...>]
+function dap_utils.make_commands()
+	vim.api.nvim_create_user_command("LaunchDebug", function(opts)
+		local conf = {
+			kind = opts.fargs[1],
+			exe = opts.fargs[2],
+			args = {},
+		}
+		for i = 3, #opts.fargs do
+			table.insert(conf.args, opts.fargs[i])
+		end
+		dap_utils.launch_debug(conf)
+	end, { nargs = "*" })
+
+	vim.api.nvim_create_user_command("LaunchDebugLast", function(_)
+		dap_utils.debug_last()
+	end, {})
+end
+
+-- TODO: maybe add a fuzzy picker for finding debug exes?
+-- TODO: maybe add launch config history? With fuzzy picker and persistence?
+
+return dap_utils
---
+++ configs/nvim/lua/plugins/dap.lua
@@ -0,0 +1,44 @@
+return {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jay-babu/mason-nvim-dap.nvim",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+}
+-- return {
+-- 	"mfussenegger/nvim-dap",
+-- 	dependencies = {
+-- 		"theHamsta/nvim-dap-virtual-text",
+-- 		"rcarriga/nvim-dap-ui",
+-- 		"nvim-neotest/nvim-nio",
+-- 	},
+-- 	config = function()
+-- 		local dap = require("dap")
+-- 		require("dapui").setup()
+-- 		require("nvim-dap-virtual-text").setup({})
+--
+-- 		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "error", linehl = "", numhl = "" })
+-- 		vim.fn.sign_define("DapBreakpointCondition", { text = "●", texthl = "@character", linehl = "", numhl = "" })
+-- 		vim.fn.sign_define("DapLogPoint", { text = "●", texthl = "@type", linehl = "", numhl = "" })
+-- 		vim.fn.sign_define("DapBreakpointRejected", { text = "●", texthl = "@function", linehl = "", numhl = "" })
+--
+-- 		-- cpp stuff
+-- 		local dap_cpp = require("codey.debug.dap-cpp")
+-- 		dap.adapters.lldb = dap_cpp.lldb_adapter
+--
+-- 		-- -- lua stuff
+-- 		-- local dap_lua = require("frank.debug.dap-lua")
+-- 		-- dap.adapters["local-lua"] = dap_lua.local_lua_adapter
+--
+-- 		-- set up my debug commands
+-- 		local dap_utils = require("codey.debug.utils")
+--
+-- 		dap_utils.register_debug_kind("cpp", dap_cpp.new_cpp_debug_config)
+-- 		dap_utils.make_commands() -- add :LaunchDebug and :LaunchDebugLast
+--
+-- 		vim.keymap.set("n", "<leader>dui", require("dapui").toggle, { desc = "Debug: [D]ap [U][I] toggle" })
+-- 	end,
+-- }
